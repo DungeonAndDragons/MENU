b6436ba9fcfb5b28e7d2929582ab5a931b0b8cc6
D&D <jerefranck1235@gmail.com>
2025-02-28 21:24:27 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function startGame() {
+    alert("Iniciando el juego...");
+}
+
+function showOptions() {
+    alert("Opciones del juego...");
+}
+
+function showCredits() {
+    alert("Créditos del juego...");
+}
